0ca4696cd828fcc90941a42f246cd7ca06f6fa04
Henry <tongsik98@gmail.com>
2024-03-10 20:42:33 -0700

    done

--- dist/main.js
+++ dist/main.js
@@ -19,81 +19,103 @@ const months = [
     "Nov",
     "Dec",
 ];
+const dbList = [];
+// date formatting function
 function formattingDate(date) {
     return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 }
+// onload
 document.addEventListener("DOMContentLoaded", () => {
     listDate.innerHTML = formattingDate(today);
+    loadFromLocalStorage();
 });
-document.addEventListener("click", () => {
-    // console.log(`${calendar.selectedDate}`);
-    listDate.innerHTML = formattingDate(calendar.selectedDate);
+// ----------------------------------------------------------------
+function loadFromLocalStorage() {
+    const savedItems = localStorage.getItem("dbList");
+    if (savedItems) {
+        const todoItems = JSON.parse(savedItems);
+        todoItems.forEach((item) => {
+            item.date = new Date(item.date);
+        });
+        dbList.push(...todoItems);
+    }
+}
+// onload
+document.addEventListener("DOMContentLoaded", () => {
+    listDate.innerHTML = formattingDate(today);
+    showData(dbList, calendar.selectedDate);
 });
+// ----------------------------------------------------------------
+// get selected date's list
+document.addEventListener("click", (e) => {
+    if (e.target.closest(".day")) {
+        listDate.innerHTML = formattingDate(calendar.selectedDate);
+        showData(dbList, calendar.selectedDate);
+    }
+});
+addBtn.addEventListener("click", () => {
+    addData(calendar.selectedDate, false, inputListEle.value);
+    inputListEle.value = "";
+    showData(dbList, calendar.selectedDate);
+});
+function addData(selectedDate, isChecked, content) {
+    const newItem = {
+        date: selectedDate,
+        isChecked: isChecked,
+        content: content,
+    };
+    dbList.push(newItem);
+    localStorage.setItem("dbList", JSON.stringify(dbList));
+}
+function showData(db, selectedDate) {
+    // erase previous data and show selected date's data
+    todoList.innerHTML = "";
+    db.forEach((item) => {
+        const index = dbList.findIndex((dbItem) => dbItem === item);
+        if (formattingDate(selectedDate) === formattingDate(item.date)) {
+            // contains checkbox, li, removeBtn
+            const listBox = document.createElement("div");
+            listBox.classList.add("listBox");
+            const checkBox = document.createElement("input");
+            checkBox.type = "checkbox";
+            checkBox.name = "checkBox";
+            const li = document.createElement("li");
+            li.textContent = item.content;
+            if (index !== -1) {
+                checkBox.checked = dbList[index].isChecked;
+                if (checkBox.checked === true) {
+                    li.classList.add("done");
+                }
+            }
+            const removeBtn = document.createElement("button");
+            removeBtn.textContent = "Remove";
+            removeBtn.classList.add("removeBtn");
+            // append elements to listBox
+            listBox.appendChild(checkBox);
+            listBox.appendChild(li);
+            listBox.appendChild(removeBtn);
+            // append div to ul
+            todoList.appendChild(listBox);
+            // add checkbox event listener after listBox is fully created and appended
+            checkBox.addEventListener("change", () => {
+                // item.isChecked = checkBox.checked;
+                if (index !== -1) {
+                    dbList[index].isChecked = checkBox.checked;
+                }
+                li.classList.toggle("done", checkBox.checked);
+                localStorage.setItem("dbList", JSON.stringify(dbList));
+            });
+            // remove button event listener
+            removeBtn.addEventListener("click", () => {
+                listBox.remove();
+                // Remove the item from dbList array
+                if (index !== -1) {
+                    dbList.splice(index, 1);
+                    showData(dbList, selectedDate); // Update displayed data
+                }
+                localStorage.setItem("dbList", JSON.stringify(dbList));
+            });
+        }
+    });
+}
 // --------------------------------------------------------------------------------------------
-// addBtn.addEventListener("click", (e) => {
-//   const inputList = inputListEle.value;
-//   addNewList(inputList);
-//   inputListEle.value = "";
-//   saveItems();
-// });
-// function addNewList(text: string) {
-//   // check box
-//   const checkBox = document.createElement("input") as HTMLInputElement;
-//   checkBox.type = "checkbox";
-//   const li = document.createElement("li") as HTMLLIElement;
-//   li.textContent = text;
-//   checkBox.addEventListener("change", () => {
-//     li.classList.toggle("done", checkBox.checked);
-//     saveItems();
-//   });
-//   // remove button
-//   const removeBtn = document.createElement("button") as HTMLButtonElement;
-//   removeBtn.textContent = "Remove";
-//   removeBtn.classList.add("removeBtn");
-//   removeBtn.addEventListener("click", () => {
-//     listBox.remove();
-//     saveItems();
-//   });
-//   // list div
-//   const listBox = document.createElement("div") as HTMLDivElement;
-//   listBox.classList.add("listBox");
-//   listBox.appendChild(checkBox);
-//   listBox.appendChild(li);
-//   listBox.appendChild(removeBtn);
-//   // append div to ul
-//   todoList.appendChild(listBox);
-// }
-// function saveItems() {
-//   let todoItems: { isChecked: boolean; text: string; savedDate: Date }[] = [];
-//   // select all divs in ul
-//   const listItems = todoList.querySelectorAll(".listBox");
-//   // save date
-//   const listDate = calendar.selectedDate;
-//   // console.log(listDate);
-//   listItems.forEach((div) => {
-//     // need to check div.children[0] is checkBox
-//     const checkBoxEle = div.children[0];
-//     let isChecked: boolean = (checkBoxEle as HTMLInputElement).checked;
-//     // could be null, used js logical operator
-//     let liContent: string = div.children[1].textContent || "";
-//     todoItems.push({ isChecked, text: liContent, savedDate: listDate });
-//   });
-//   // for (let i = 0; i < todoItems.length; i++) {
-//   //   console.log(`${i}: ${todoItems[i].savedDate}`);
-//   // }
-//   localStorage.setItem("todoItems", JSON.stringify(todoItems));
-// }
-// window.addEventListener("load", () => {
-//   const savedItems = localStorage.getItem("todoItems");
-//   if (savedItems) {
-//     const todoItems: { isChecked: boolean; text: string; savedDate: Date }[] =
-//       JSON.parse(savedItems);
-//     todoItems.forEach((item) => {
-//       addNewList(item.text);
-//       const listBox = todoList.lastElementChild as HTMLDivElement;
-//       const checkBox = listBox.children[0] as HTMLInputElement;
-//       checkBox.checked = item.isChecked;
-//       listBox.children[1].classList.toggle("done", item.isChecked);
-//     });
-//   }
-// });
